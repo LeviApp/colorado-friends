eec07919c68d1a50d8ba4842a5c5fcf673bd4f75
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-06-03 22:21:54 -0600

    sections VISION and AIMS complete

--- colorado-friends/src/components/Mission.js
+++ colorado-friends/src/components/Mission.js
@@ -4,6 +4,7 @@ import '../SASS/mission.sass';
 function Mission(props) {
     return (
         <div className="mission">
+            <section>
             <h2>MISSION</h2>
             <ul>
                 <li>To bring and uplift missing talents of children  within the community</li>
@@ -12,7 +13,27 @@ function Mission(props) {
                 <li>To instill in children the ideas of a good God-fearing sportsman, honesty, loyalty and teamwork in order for them to become strong, healthy, well-adjusted members of the community</li>
                 <li>To bring African culture and voice to the forefront</li>
             </ul>
+            </section>
 
+            <section>
+            <h2>VISION</h2>
+            <ul>
+                <li>To experience culture among all ethnic groups, specifically celebrating and educating people of African culture.</li>
+                <li>To raise awareness of dance as a tool to facilitate heart and well-being of individuals in the music industry.</li>
+                <li>To make the heritage of African culture, dance, songs, and rhythm available to all</li>
+                <li>To disestablish stereotypes of both Europeans and Blacks</li>
+            </ul>
+            </section>
+
+            <section>
+            <h2>AIMS</h2>
+            <ul>
+                <li>To develop artistic and strong works that are giving and inclusive to our audiences</li>
+                <li>To seamlessly use tradition and neo-traditional African movement vocabulary to create work developed from the Africanist movement, which reflects our social culture values and realities in global contexts</li>
+                <li>To ensure that discipline and multitasking skills develop through African dance and the rest of the world culture is firmly transferred to other areas of people , lives be it academic carrier or private.</li>
+                <li>To ensure that the world's cultures are transferred to every part of people's lives.</li>
+            </ul>
+            </section>
         </div>
     );
   }
